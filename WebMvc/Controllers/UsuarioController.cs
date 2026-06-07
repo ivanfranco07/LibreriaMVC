6256b71fd31e7c79edc91b6cabd5982539b4b73b
ivanfranco07 <ivan.franco07@hotmail.com>
2026-06-07 01:04:56 -0300

    Interfaz de login y sign up

---
+++ WebMvc/Controllers/UsuarioController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebMvc.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: UsuarioController
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+        public async Task<IActionResult> SignUp()
+        {
+            return View();
+        }
+    }
+}
